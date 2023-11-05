bea416547bdb257f891a8b0d2b603d021cea4454
alekseev31446 <alekseev3446@gmail.com>
2023-11-05 23:57:18 +0200

    added get request

---
+++ src/main/java/MongoDBConfig.java
@@ -0,0 +1,33 @@
+import com.mongodb.MongoClient;
+import com.mongodb.MongoClientURI;
+import com.mongodb.client.MongoDatabase;
+import org.bson.codecs.configuration.CodecRegistries;
+import org.bson.codecs.configuration.CodecRegistry;
+import org.bson.codecs.pojo.PojoCodecProvider;
+
+public class MongoDBConfig {
+    private final MongoClient mongoClient;
+    private final MongoDatabase database;
+
+    public MongoDBConfig() {
+        MongoClientURI uri = new MongoClientURI("mongodb://localhost:27017");
+
+        CodecRegistry codecRegistry = CodecRegistries.fromRegistries(
+            MongoClient.getDefaultCodecRegistry(),
+            CodecRegistries.fromProviders(PojoCodecProvider.builder().automatic(true).build())
+        );
+
+        mongoClient = new MongoClient(uri);
+        database = mongoClient.getDatabase("university").withCodecRegistry(codecRegistry);
+    }
+
+    public void close() {
+        mongoClient.close();
+    }
+
+	public MongoDatabase getDatabase() {
+		return database;
+	}
+}
+
+
--- src/main/java/StudentCrud.java
+++ src/main/java/StudentCrud.java
@@ -6,21 +6,58 @@ import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.bson.conversions.Bson;
+import org.bson.types.ObjectId;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.mongodb.client.MongoCollection;
+import com.mongodb.client.MongoDatabase;
+import com.mongodb.client.model.Filters;
 
 @WebServlet("/StudentCrud")
 public class StudentCrud extends HttpServlet {
 	private static final long serialVersionUID = 1L;
+	
+	private final MongoDBConfig mongoDBConfig;
 
     public StudentCrud() {
         super();
+        mongoDBConfig = new MongoDBConfig();
     }
 
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		response.getWriter().append("Served at: ").append(request.getContextPath());
-	}
+		String idParam = request.getParameter("id");
 
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		doGet(request, response);
+        ObjectMapper objectMapper = new ObjectMapper();
+        MongoDatabase database = mongoDBConfig.getDatabase();
+        MongoCollection<Student> collection = database.getCollection("students", Student.class);
+
+        if (idParam != null) {
+            try {
+                ObjectId studentId = new ObjectId(idParam);
+                Bson filter = Filters.eq("_id", studentId);
+                Student student = collection.find(filter).first();
+
+                if (student != null) {
+                    String studentJson = objectMapper.writeValueAsString(student);
+                    response.setContentType("application/json");
+                    response.getWriter().print(studentJson);
+                } else {
+                    response.setStatus(HttpServletResponse.SC_NOT_FOUND);
+                }
+            } catch (IllegalArgumentException e) {
+                response.setStatus(HttpServletResponse.SC_BAD_REQUEST);
+            }
+        } else {
+            List<Student> students = collection.find().into(new ArrayList<>());
+            String studentsJson = objectMapper.writeValueAsString(students);
+            response.setContentType("application/json");
+            response.getWriter().print(studentsJson);
+        }
 	}
 
 }
